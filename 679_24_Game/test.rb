25a5e717e66ef850472ea98ade07f4eeb7eaea59
Artem <artemti0@gmail.com>
2025-08-20 16:43:16 +0300

    feat: hard: 679 - 24 game

---
+++ 679_24_Game/test.rb
@@ -0,0 +1,54 @@
+@operations = [
+  ->(a, b) { a + b },
+  ->(a, b) { a - b },
+  ->(a, b) { a * b },
+  ->(a, b) { b - a },
+  ->(a, b) { b.zero? ? nil : a / b },
+  ->(a, b) { a.zero? ? nil : b / a }
+]
+
+def rec_combine(nums)
+  return nums[0].round(8) == 24 if nums.length == 1
+
+  (0...nums.length).each do |a_index|
+    a = nums[a_index]
+
+    ((a_index + 1)...nums.length).each do |b_index|
+      b = nums[b_index]
+
+      new_nums = nums.dup
+      new_nums.delete_at(b_index)
+      new_nums.delete_at(a_index)
+
+      @operations.each do |op|
+        num = op.call(a, b)
+        next if num.nil?
+
+        found = rec_combine(new_nums.push(num))
+
+        new_nums.pop
+
+        return true if found
+      end
+    end
+  end
+
+  false
+end
+
+# @param {Integer[]} cards
+# @return {Boolean}
+def judge_point24(cards)
+  rec_combine(cards.map(&:to_f))
+end
+
+# cards = [1,7,4,5]
+# p judge_point24(cards)
+
+#
+# cards = [1,9,1,2]
+# p judge_point24(cards)
+
+cards = [3, 3, 8, 8]
+p judge_point24(cards)
+
